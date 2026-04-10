d2ca31ae6680154d6b59af198c2f687c94645aa3
Mikolaj Kadlubowski <mk149300@student.sgh.waw.pl>
2026-04-10 08:31:42 +0200

    First commit, loading data

---
+++ data_loader.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+
+def load_data():    
+
+    df = pd.read_csv('support2.csv')
+    return df
---
+++ main.py
@@ -0,0 +1,4 @@
+from data_loader import load_data
+
+df = load_data()
+print(df.head())
